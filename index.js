94efce2e1e95d41616a96f94ad890777a7cd8c44
mjaaks <mjaakss@pm.me>
2025-10-02 09:11:45 +1000

    Added themes and theme switching

---
+++ index.js
@@ -0,0 +1,24 @@
+let themeNames = [];
+let currentIndex = 0;
+let themes = {}
+
+async function loadThemes(){
+    const res = await fetch('assets/themes.json');
+    themes = await res.json();
+    themeNames = Object.keys(themes);
+    applyTheme(themeNames[0]);
+}
+
+function applyTheme(themeName){
+    const theme = themes[themeName];
+    for (const [key, value] of Object.entries(theme)){
+        document.documentElement.style.setProperty(`--${key}`, value)
+    }
+}
+
+document.getElementById('colour-button').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % themeNames.length;
+    applyTheme(themeNames[currentIndex]);
+});
+
+loadThemes();
